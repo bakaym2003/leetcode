ae9d0cadca3263226ca78b33b071bb5adc45cd25
bakaim23 <bakay.m32@gmail.com>
2021-08-05 21:38:43 +0600

    Find Global Variable

---
+++ LeetcodeSolutions/src/main/java/com/leetcode/App2.java
@@ -0,0 +1,12 @@
+package com.leetcode;
+
+public class App2 {
+    public class App
+    {
+        public static void main( String[] args )
+        {
+            System.out.println( "Hello World!" );
+        }
+    }
+
+}
